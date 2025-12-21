efd053af9407392081b4eb5d09214871b8cd1c35
irbitnik <249591115+irbitnik@users.noreply.github.com>
2025-12-21 17:12:39 +0500

    Несколько функций

--- Rarecrows/func.py
+++ Rarecrows/func.py
@@ -2,31 +2,52 @@ import random as rand
 import pyautogui as pag
 import time as t
 
+# TODO Обернуть в класс
 
-def rand_click(coord: tuple) -> None:  # TODO Обернуть в класс
+
+def first_click() -> None:
+    x = rand.randint(85, 140)
+    y = rand.randint(195, 240)
+    pag.click(x, y, duration=rand.uniform(0.5, 1), tween=pag.easeInOutCubic)
+
+
+def rand_click(coord: tuple) -> None:
 
     x = rand.randint(coord[0], coord[1])
     y = rand.randint(coord[2], coord[3])
-    pag.click(x, y, duration=rand.uniform(0.2, 0.4), tween=pag.easeInOutCubic) # TODO Добавить еще один клик с небольшим смещением
+    pag.click(x, y, duration=rand.uniform(0.1, 0.3), tween=pag.easeInOutCubic)
 
 
-def rand_event(number):
+def add_move() -> None:
+    if rand.random() < 0.1:
+        pag.moveRel(rand.randint(-10, 10), rand.randint(-10, 10), duration=rand.uniform(0.1, 0.2))
+        if rand.random() < 0.2:
+            pag.click()
+
+
+def rand_event(number: int) -> None:
     match number:
         case 1:
-            pag.moveRel(rand.randint(-40, 40), rand.randint(-40, 40), duration=rand.uniform(0.2, 0.4))
+            pag.moveRel(rand.randint(-40, 40), rand.randint(-40, 40), duration=rand.uniform(0.1, 0.3))
             t.sleep(rand.randint(1, 5))
         case 2:
-            pag.moveRel(rand.randint(-40, 40), rand.randint(-40, 40), duration=rand.uniform(0.2, 0.4))
+            pag.moveRel(rand.randint(-40, 40), rand.randint(-40, 40), duration=rand.uniform(0.1, 0.3))
             t.sleep(rand.randint(10, 20))
         case 3:
-            pag.moveTo(rand.randint(10, 450), rand.randint(300, 600), duration=rand.uniform(1, 3), tween=pag.easeInOutCubic)
+            pag.moveTo(rand.randint(10, 450), rand.randint(300, 600), duration=rand.uniform(0.4, 1), tween=pag.easeInOutCubic)
+            t.sleep(rand.uniform(1, 2))
         case 4:
-            pag.moveTo(rand.randint(10, 1000), rand.randint(400, 750), duration=rand.uniform(1, 3), tween=pag.easeInOutCubic)
+            pag.moveTo(rand.randint(10, 1000), rand.randint(400, 750), duration=rand.uniform(0.4, 1), tween=pag.easeInOutCubic)
+            t.sleep(rand.uniform(1, 2))
         case 5:
             pag.click(rand.randint(500, 800), rand.randint(200, 700), clicks=rand.randint(1, 4), interval=rand.uniform(0.4, 1),
-                      duration=rand.uniform(1, 3), tween=pag.easeInOutCubic)
-        case _:
-            pag.moveRel(rand.randint(-10, 10), rand.randint(-40, 40), duration=rand.uniform(0.2, 0.4))
-            pag.moveRel(rand.randint(-10, 10), rand.randint(-40, 40), duration=rand.uniform(0.2, 0.4))
-            pag.moveRel(rand.randint(-10, 10), rand.randint(-40, 40), duration=rand.uniform(0.2, 0.4))
+                      duration=rand.uniform(0.4, 1), tween=pag.easeInOutCubic)
             t.sleep(rand.uniform(1, 2))
+        case _:
+            for _ in range(rand.randint(1, 4)):
+                pag.moveRel(rand.randint(-10, 10), rand.randint(-10, 10), duration=rand.uniform(0.2, 0.4))
+            t.sleep(rand.uniform(1, 2))
+
+
+def find_image(image: str, coord: tuple) -> None:
+    pag.locateOnScreen(image, grayscale=True, confidence=0.8, region=coord)
--- Rarecrows/main_rarecrows.py
+++ Rarecrows/main_rarecrows.py
@@ -8,41 +8,48 @@ import time as t
 
 
 if __name__ == '__main__':  # TODO Сделать десктопным
+
     print('Для запуска скрипта нажмите "Enter"')  # TODO Несколько потоков
     print('Для завершения скрипта нажмите "Esc"')
     key.wait('enter', suppress=True)
+
+    count = 0
     ops = False
-    while True:
+    while count <= set.shovels/5:
 
-        x = rand.randint(85, 140)
-        y = rand.randint(195, 240)
-        pyautogui.click(x, y, duration=rand.uniform(0.5, 1), tween=pyautogui.easeInOutCubic)
+        func.first_click()
 
-        for i in set.coord:  # TODO Оформить поиск картинок как функции
+        for i in set.coord:
             try:
-                pyautogui.locateOnScreen('image/ops.png', grayscale=True, confidence=0.8, region=(0, 260, 480, 240))
+                func.find_image('image/ops.png', (0, 260, 480, 240))
                 func.rand_click((30, 445, 445, 480))
                 print('Обработка плохого интернет соединения')
                 ops = True
                 t.sleep(rand.randint(10, 20))
             except pyautogui.ImageNotFoundException:
                 try:
-                    pyautogui.locateOnScreen('image/load.png', grayscale=True, confidence=0.8, region=(0, 260, 480, 240))
+                    func.find_image('image/load.png', (0, 260, 480, 240))
                     t.sleep(rand.randint(10, 20))
                 except pyautogui.ImageNotFoundException:
                     if ops:
                         func.rand_click((425, 460, 630, 660))
                         t.sleep(rand.randint(3, 5))
                         func.rand_click((320, 365, 625, 670))
-                        print('Снова выбрал редиску')
+                        print('Выбрал редиску')
                         ops = False
                     if not ops:
                         try:
-                            pyautogui.locateOnScreen('image/gnomes.png', grayscale=True, confidence=0.8, region=(0, 310, 80, 150))
-                            func.rand_click(i)  # TODO Добавить "промахивание" мышкой по грядке
+                            func.find_image('image/gnomes.png', (0, 310, 80, 150))
+                            func.rand_click(i)
+                            func.add_move()
                             if key.is_pressed("esc"):
                                 sys.exit()
                         except pyautogui.ImageNotFoundException:
                             print("Не могу найти гномов")
                             t.sleep(rand.randint(30, 50))
+
         func.rand_event(rand.randint(1, 10))
+
+        count += 1
+        if count % 20 == 0:
+            print(f'Потрачено {count*5} лопат')
